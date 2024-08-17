80a4ae9c5f19b7efbd81f414ec30502123c55bf4
JamesBerlioz <mavlut18@gmail.com>
2024-08-18 00:36:58 +0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
