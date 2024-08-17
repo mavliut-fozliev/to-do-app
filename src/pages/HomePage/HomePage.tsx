71f872a141589d3782928fd64940928f7844419b
JamesBerlioz <mavlut18@gmail.com>
2024-08-18 01:42:37 +0300

    feat: add TodoTitle and AddTaskForm to Home page and give styles

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import AddTaskForm from "../../components/AddTaskForm/AddTaskForm";
+import TodoTitle from "../../components/TodoTitle/TodoTitle";
+import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const addTask = (taskContent: string) => {
+    setTasks([...tasks, taskContent]);
+  };
+
+  return (
+    <div className={styles["box"]}>
+      <TodoTitle />
+      <AddTaskForm onAddTask={addTask} />
+    </div>
+  );
 };
 
 export default HomePage;
