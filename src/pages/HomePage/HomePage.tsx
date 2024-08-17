9317eed7a82b1046d0adcc18533833dc6a0746f5
JamesBerlioz <mavlut18@gmail.com>
2024-08-18 02:15:13 +0300

    feat: add TaskList to page and update addTask function to create unique ID for every task

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import AddTaskForm from "../../components/AddTaskForm/AddTaskForm";
 import TodoTitle from "../../components/TodoTitle/TodoTitle";
 import styles from "./HomePage.module.scss";
+import TaskList from "../../components/TaskList/TaskList";
+import { Task } from "../../types";
+import { v4 as uuidv4 } from "uuid";
 
 const HomePage: React.FC = () => {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const addTask = (taskContent: string) => {
-    setTasks([...tasks, taskContent]);
+    const newTask: Task = {
+      id: uuidv4(),
+      content: taskContent,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
   };
 
   return (
     <div className={styles["box"]}>
       <TodoTitle />
       <AddTaskForm onAddTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
 };
