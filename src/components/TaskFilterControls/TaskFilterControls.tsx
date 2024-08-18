316da611984df48810ba4cbf6e1cf1b936a75891
JamesBerlioz <mavlut18@gmail.com>
2024-08-18 03:28:36 +0300

    fix: add key to array item

--- src/components/TaskFilterControls/TaskFilterControls.tsx
+++ src/components/TaskFilterControls/TaskFilterControls.tsx
@@ -11,7 +11,7 @@ interface TaskFilterControlsProps {
 
 const TaskFilterControls: React.FC<TaskFilterControlsProps> = ({ currentFilter, onFilterChange, onClearCompleted }) => {
   const getFilterButton = (filter: TaskFilter) => (
-    <Button onClick={() => onFilterChange(filter)} color={currentFilter === filter ? "primary" : "inherit"}>
+    <Button key={filter} onClick={() => onFilterChange(filter)} color={currentFilter === filter ? "primary" : "inherit"}>
       {filter}
     </Button>
   );
